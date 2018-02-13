409dbb56c36e261ff0104a1c998cced3268feabf
송형찬 <worldviator1@gmail.com>
2018-02-13 19:32:03 +0900

    join.xml + joinController

---
+++ src/com/bkproj/web/JoinController.java
@@ -0,0 +1,13 @@
+package com.bkproj.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JoinController {
+	@RequestMapping("member/joinAgree.whpr")
+	public String join() throws Exception{
+		
+		return "/member/joinAgree.jsp";
+	}
+}
